08de54fb7be4903cce98a55183130b70eb2438a5
isuapp <isaacmarti.g@gmail.com>
2022-06-13 10:00:17 +0200

    Cambio del html y del logo

--- dia-1/src/App.js
+++ dia-1/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+const modulos =[
+  {
+    name:'HTML',
+    curso:'Hack a Boss Bootcamp'
+  },
+  {
+    name:'CSS',
+    curso:'Hack a Boss Bootcamp'
+  },
+  {
+    name:'JS',
+    curso:'Hack a Boss Bootcamp'
+  },
+  {
+    name:'NODE',
+    curso:'Hack a Boss Bootcamp'
+  },
+  {
+    name:'REACT',
+    curso:'Hack a Boss Bootcamp'
+  }
+];
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <h1>React Hack a boss</h1>
+        <article>
+          <ul>
+          <li>
+            <article>
+              <h3>{modulos[0].name}</h3>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>{modulos[1].name}</h3>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>{modulos[2].name}</h3>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>{modulos[3].name}</h3>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>{modulos[4].name}</h3>
+            </article>
+          </li>
+          </ul>
+        </article>
+      </main>
     </div>
   );
 }
