820d70b1dd083c00f33582ed5231488e70312453
isuapp <isaacmarti.g@gmail.com>
2022-06-15 12:38:12 +0200

    ejercicio done

--- dia-2/ejercicios/chatroom/src/App.js
+++ dia-2/ejercicios/chatroom/src/App.js
@@ -3,13 +3,45 @@ import './App.css';
 import ChatRoom from './components/ChatRoom';
 import ChatRoomHeader from './components/ChatRoomHeader';
 import Message from './components/Message';
+import { useState } from 'react';
+import messagesDB from './messages.json'
+import SendMessage from './components/SendMessage';
+import { nanoid } from 'nanoid'
 
 function App() {
+
+    const [messages, setMessages] = useState(messagesDB);
+
+    const allMessages = messages.map((itemMessage)=>{
+       return  <Message message={itemMessage} key={itemMessage.id}/>
+    })
+    
+    const sendMessage = (e)=>{
+        e.preventDefault();
+        
+        let inputValue = e.target.elements.message;
+        let actualDate = new Date();
+        
+        const bodyMessage = {
+            id:nanoid(),
+            author: 456327,
+            body: inputValue.value,
+            date: actualDate.toLocaleTimeString()
+        }
+
+        setMessages([...messages, bodyMessage]);
+        inputValue.value = '';
+    }
+
+    
     return (
         <div>
             <ChatRoom>
                 <ChatRoomHeader></ChatRoomHeader>
-                <Message />
+                <ul>
+                    {allMessages}
+                </ul>
+                <SendMessage send={sendMessage} />
             </ChatRoom> 
         </div>
     );
